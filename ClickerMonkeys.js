a2d06519daa3e7708f2055e1f3d22ca7615fcb69
smadmin <smadmin@localhost.localdomain>
2014-11-01 11:03:47 -0700

    Refactored isGuilded to use for loop instead. Renamed app, id, name, upped version, corrected author to authors. Added missing semi-colons/removed extra semi-colons. Cleaned up overloaded functions. Removed unused function. Various other changes.

--- ClickerMonkeys.js
+++ ClickerMonkeys.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @id          ZininsClickerMonkeys
-// @name        Zinin's ClickerMonkeys
+// @id          ClickerMonkeys
+// @name        Clicker Monkeys
 // @namespace   .
-// @version     1.0
-// @author      Zininzinin
+// @version     1.1
+// @authors     Zininzinin, unv_annihilator
 // @description Trying to automate ALL THE THINGS with clicker heroes
 // @include     http://www.clickerheroes.com/
 // @grant       none
@@ -22,45 +22,45 @@
         var loadTimeout = 10000;
         
         //Original credit goes to 0XYGeN64, JackPS9, and VikeStep
-        //Edited by zininzinin
-
+        //Edited by zininzinin and unv_annihilator
+        
         //Do Not Change
         var baseCosts = [10, 50, 250, 1000, 4000, 20000, 100000, 400000, 2500000, 15000000, 100000000, 800000000, 6500000000, 50000000000, 450000000000, 4000000000000, 36000000000000, 320000000000000, 2700000000000000, 24000000000000000, 300000000000000000, 9000000000000000000, 350000000000000000000, 1.4e+22, 4.19999999999999e+24, 2.1e+27];
         var zoneTimer = 0;
         var currentZone = 0;
-
+        
         //Set What you want as your max level
         var MLevel = 150;
-
+        
         //IDs of the gilded heroes (Cid is 0)
         var guildedList = [6];
-
+        
         //set to 1 if you do not have dogcog and set to 0.5 if max level.
-        var dogcog = 0.5;
-
+        var dogcog = 1;
+        
         //Basically how fast it levels up heroes. Set interval higher for slower repeats. Recommended minimum value 10.
         var purchaseInterval = 100;
-
+        
         //How often to check if you should ascend in miliseconds. Default is 30 seconds.
         var ascendInterval = 30000;
-
+        
         //How long (in seconds) to wait for changezone before ascending. Example: If timeout is 60 seconds and it takes you longer than 60 seconds to defeat a zone you will ascend.
         var ascendTimeout = 60;
-
+        
         //Will not ascend before you have reached this zone
         var minAscendZone = 100;
         
         //How often to try to buy all upgrades
         var upgradeInterval = 10000;
-
+        
         //***** implement app here *****
         var App = {
-            name: "zininzinin's Example App",
+            name: "Clicker Monkeys",
             onPlaying: function () {
                 setInterval(purchaseHighest,purchaseInterval);
                 setInterval(JSMod.buyAllAvailableUpgrades,upgradeInterval);
                 setInterval(tryAscend,ascendInterval);
-                JSMod.setProgressMode(true);
+                // JSMod.setProgressMode(true);
             },
             onSelectedZone: function (zone) {
                 zoneTimer = Date.now();
@@ -68,36 +68,22 @@
                 console.log("New zone: " + zone + " at time " + zoneTimer);
             }
         };
-
+        
         function tryAscend() {
-            var timeout = Date.now() - zoneTimer / 1000
+            var timeout = Date.now() - zoneTimer / 1000;
             console.log("Trying to ascend. Timeout is " + timeout);
-            if (currentZone >= minAscendZone && (timeout > ascendTimeout))
-                {
-                    JSMod.ascend();
-                    JSMod.setProgressMode(true);
-                }
-        }
-
-        function onSelectedZone(zone){
-            zoneTimer = Date.now();
-            currentZone = zone;
-            console.log("New zone: " + zone + "!");
-        } 
-         
-        function calculateHeroCost(id, level) {
-            if (id === 0 && level <= 15) {
-                return Math.floor((5 + level) * Math.pow(1.07, level) * dogcog);
-            } else if (id === 0) {
-                return Math.floor(20 * Math.pow(1.07, level) * dogcog);
-            } else {
-                return Math.floor(baseCosts[id] * Math.pow(1.07, level) * dogcog);
+            if (currentZone >= minAscendZone && (timeout > ascendTimeout)) {
+                JSMod.ascend();
+                // JSMod.setProgressMode(true);
             }
         }
-
+        
         function calculateHeroCost(id) {
-            var save = JSON.parse(JSMod.getUserData());
-            var level = JSON.parse(JSMod.getUserData()).heroCollection.heroes[id+1].level
+			var level = JSON.parse(JSMod.getUserData()).heroCollection.heroes[id+1].level;
+			return calculateHeroCost(id, level);
+        }
+        
+		function calculateHeroCost(id, level) {
             if (id === 0 && level <= 15) {
                 return Math.floor((5 + level) * Math.pow(1.07, level) * dogcog);
             } else if (id === 0) {
@@ -106,58 +92,50 @@
                 return Math.floor(baseCosts[id] * Math.pow(1.07, level) * dogcog);
             }
         }
-
+		
         function canPurchaseHero(id) {
-            var save = JSON.parse(JSMod.getUserData());
-            return (save.gold > calculateHeroCost(id)) && (save.heroCollection.heroes[id+1].level < MLevel || isGuilded(guildedList, id));
+			return canPurchaseHero(id, (JSON.parse(JSMod.getUserData())).gold);
         }
-
+        
         function canPurchaseHero(id, gold) {
             return (gold > calculateHeroCost(id)) && (save.heroCollection.heroes[id+1].level < MLevel || isGuilded(guildedList, id));
         }
-
+        
         function isGuilded(guildedList, heroID) {
-            var i = guildedList.length;
-            while (i--) {
-               if (guildedList[i] == heroID) {
-                   return true;
-               }
-            }
-            return false;
-        }
-         
-        function purchaseCheapest() {
-            var save = JSON.parse(JSMod.getUserData());
-            var heroCosts = [];
-            var currentGold = save.gold;
-            for (var i = 0; i < 26; i++) {
-                if(save.heroCollection.heroes[i+1].level < MLevel || isGuilded(guildedList, i))
-                {
-                    heroCosts[i] = calculateHeroCost(i, save.heroCollection.heroes[i+1].level);
+			for (var i = 1; i < guildedList.length; i++) {
+				if (guildedList[i] == heroID) {
+                    return true;
                 }
-                else
-                {
+			}
+			return false;
+        }
+		
+        function purchaseCheapest() {
+            var heroCosts = [];
+            var currentGold = JSON.parse(JSMod.getUserData()).gold;
+			var heroCollection = JSON.parse(JSMod.getUserData()).heroCollection;
+            for (var i = 0; i < 26; i++) {
+                if (heroCollection.heroes[i+1].level < MLevel || isGuilded(guildedList, i)) {
+                    heroCosts[i] = calculateHeroCost(i, heroCollection.heroes[i+1].level);
+                } else {
                     heroCosts[i] = Number.MAX_VALUE;
                 }
             }
-            if (currentGold > Math.min.apply(Math,heroCosts))
-            {
+            if (currentGold > Math.min.apply(Math,heroCosts)) {
                 JSMod.levelHero(heroCosts.indexOf(Math.min.apply(Math,heroCosts)) + 1);
             }
         }
-
+        
         function purchaseHighest() {
-            var save = JSON.parse(JSMod.getUserData());
-            var currentGold = save.gold;
             var heroCost;
-            for (var i = 25; i >= 0; i--) {
-                if(save.heroCollection.heroes[i+1].level < MLevel || isGuilded(guildedList, i))
-                {
-                    heroCost = calculateHeroCost(i, save.heroCollection.heroes[i+1].level);
-                    if (currentGold > heroCost)
-                    {
+			var currentGold = JSON.parse(JSMod.getUserData()).gold;
+			var heroCollection = JSON.parse(JSMod.getUserData()).heroCollection;
+            for (var i = 25; i > 0; i--) {
+                if(heroCollection.heroes[i+1].level < MLevel || isGuilded(guildedList, i)) {
+                    heroCost = calculateHeroCost(i, heroCollection.heroes[i+1].level);
+                    if (currentGold > heroCost) {
                         JSMod.levelHero(i + 1);
-                        return
+                        break;
                     }
                 }
             }
@@ -178,8 +156,8 @@
         }
         
         init();
-    }
-
+    };
+    
     function inject(func) {
         var script = document.createElement("script");
         script.setAttribute("type", "text/javascript");
